53791e5f0e43a9d8531797e65e3aa2ef507eb28e
Sjur Grønnevik Wroldsen <sjurgw@stud.ntnu.no>
2019-03-19 00:37:44 +0100

    Fixed format of print

--- treningsdagbok/Oktinfo.java
+++ treningsdagbok/Oktinfo.java
@@ -13,18 +13,18 @@ public class Oktinfo extends DBConn{
             
             ResultSet rs = stmt.executeQuery(query);
             int nr = 1;
-            String leftAlignFormat = "| %-5d | %-10s | %-23s | %-8s | %-14s | %-10d | %-7d | %-6d |%n";
+            String leftAlignFormat = "| %-5d | %-10s | %-23s | %-8s | %-14s | %-10d | %-7d | %-20s |%n";
             System.out.println("Resultatliste for klasse Treningsokt");
-            System.out.format("+-------+------------+-------------------------+----------+----------------+------------+---------+--------+%n");
-            System.out.format("| OktID | Dato       | Tidspunkt               | Varighet | Personlig form | Prestasjon | NotatID | Formal |%n");
-            System.out.format("+-------+------------+-------------------------+----------+----------------+------------+---------+--------+%n");
+            System.out.format("+-------+------------+-------------------------+----------+----------------+------------+---------+----------------------+%n");
+            System.out.format("| OktID | Dato       | Tidspunkt               | Varighet | Personlig form | Prestasjon | NotatID | Formal               |%n");
+            System.out.format("+-------+------------+-------------------------+----------+----------------+------------+---------+----------------------+%n");
             while (nr <= N) {
                 if (!rs.next()){
                     break;
                 }
             System.out.format(leftAlignFormat, rs.getInt("OktID"), rs.getDate("Dato"), rs.getTimestamp("Tidspunkt"), rs.getTime("Varighet"), rs.getInt("PersonligForm"), rs.getInt("Prestasjon"), rs.getInt("NotatID"), rs.getString("Treningsformal"));
          }
-         System.out.format("+-------+------------+-------------------------+----------+----------------+------------+---------+--------+%n");
+         System.out.format("+-------+------------+-------------------------+----------+----------------+------------+---------+----------------------+%n");
           } catch (Exception e) {
              System.out.println("db error during select of treningsokt = "+e);
          }
